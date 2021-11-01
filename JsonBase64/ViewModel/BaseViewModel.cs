55e7213b2b8df85a8593303e0de1aeedab64b10e
David-barce <71525685+David-barce@users.noreply.github.com>
2021-11-01 17:37:23 -0600

    cambios

---
+++ JsonBase64/ViewModel/BaseViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using JsonBase64.Model;
+
+namespace JsonBase64.ViewModel
+{
+    public class BaseViewModel<T> : INotifyPropertyChanged
+    {
+        public T Model { get; set; }
+
+        public BaseViewModel(T model)
+        {
+            Model = model;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnpropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public T EncripOrDecriptData(bool isEncript = true)
+        {
+            Type myType = Model.GetType();
+            var newModel = Activator.CreateInstance(myType);
+            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+            foreach(PropertyInfo prop in props)
+            {
+                object propValue = prop.GetValue(Model);
+
+                if (propValue != null && propValue.GetType() == typeof(string))
+                {
+                    if(isEncript)
+                        prop.SetValue(newModel, Helper.Base64Encrypter.Encriptar(propValue.ToString()));
+                    else
+                        prop.SetValue(newModel, Helper.Base64Encrypter.Decriptar(propValue.ToString()));
+
+                }
+
+            }
+            return (T)newModel;
+        }
+
+        public string ModelToJson(T model)
+        {
+            return JsonSerializer.Serialize(model);
+
+        }
+    }
+}
---
+++ JsonBase64/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,103 @@
+﻿using JsonBase64.Model;
+using JsonBase64.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JsonBase64.ViewModel
+{
+    public class RegisterViewModel : BaseViewModel<Model.Usuario>
+    {
+        private string _jsonResult;
+        private Command _registerCommand;
+        private UsuariosService usuariosService;
+
+        public RegisterViewModel(Usuario model = null ) : base(model)
+        {
+            if(model == null)
+            {
+                Model = new Usuario();
+            }
+
+            usuariosService = new UsuariosService();
+        }
+
+        public string UserName
+        {
+            get => Model.UserName;
+            set
+            {
+                if (string.Equals(value, Model.UserName)) return;
+                Model.UserName = value;
+                OnpropertyChanged();
+            }
+        }
+        public string Password
+        {
+            get => Model.Password;
+            set
+            {
+                if (string.Equals(value, Model.Password)) return;
+                Model.Password = value;
+                OnpropertyChanged();
+            }
+        }
+        public string Name
+        {
+            get => Model.Name;
+            set
+            {
+                if (string.Equals(value, Model.Name)) return;
+                Model.Name = value;
+                OnpropertyChanged();
+            }
+        }
+        public string Ap1
+        {
+            get => Model.Ap1;
+            set
+            {
+                if (string.Equals(value, Model.Ap1)) return;
+                Model.Ap1 = value;
+                OnpropertyChanged();
+            }
+        }
+        public string Ap2
+        {
+            get => Model.Ap2;
+            set
+            {
+                if (string.Equals(value, Model.Ap2)) return;
+                Model.Ap2 = value;
+                OnpropertyChanged();
+            }
+        }
+        public Command RegisterCommand
+        {
+            get => _registerCommand ??
+                (_registerCommand = new Command(RegisterActionAsync));
+                
+        }
+
+        public string JsonResult
+        {
+            get => _jsonResult;
+            set
+            {
+                if (string.Equals(_jsonResult, value)) return;
+                _jsonResult = value;
+                OnpropertyChanged();
+            }
+        }
+
+        private async void RegisterActionAsync()
+        {
+            Usuario newObject = EncripOrDecriptData(true);
+            //JsonResult = ModelToJson(newObject);
+
+            JsonResult = await usuariosService?.RegisterAsync(newObject);
+        }
+    }
+}
